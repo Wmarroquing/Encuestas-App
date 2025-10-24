65a14943eceb62db97f446d2d68b4f8dbe8858f7
Walter Marroquin <walter.marroquin@nucodiga.com>
2025-10-24 13:57:56 -0600

    Creacion de arquitectura modular del proyecto

---
+++ lib/common/theme/custom_theme.dart
@@ -0,0 +1,60 @@
+import 'package:devel_app/common/theme/custom_colors.dart';
+import 'package:flutter/material.dart';
+
+class CustomTheme {
+  static ThemeData get lightTheme => ThemeData(
+    useMaterial3: true,
+    fontFamily: 'Inter',
+    colorScheme: ColorScheme.fromSeed(seedColor: CustomColors.primary).copyWith(
+      primary: CustomColors.primary,
+      primaryContainer: CustomColors.primary.withValues(alpha: 0.8),
+      secondary: CustomColors.accent,
+      secondaryContainer: CustomColors.accent.withValues(alpha: 0.8),
+      surface: CustomColors.backgroundSecondary,
+      error: CustomColors.error,
+      onPrimary: Colors.white,
+      onSecondary: Colors.black,
+      onSurface: CustomColors.textPrimary,
+      onError: Colors.white,
+      brightness: Brightness.light,
+    ),
+    inputDecorationTheme: InputDecorationTheme(
+      border: outlineInputBorder(CustomColors.gray),
+      focusedBorder: outlineInputBorder(CustomColors.primary),
+      errorBorder: outlineInputBorder(CustomColors.error),
+      enabledBorder: outlineInputBorder(CustomColors.gray),
+      disabledBorder: outlineInputBorder(CustomColors.gray),
+      errorStyle: const TextStyle(color: CustomColors.error),
+      hintStyle: const TextStyle(
+        fontSize: 13,
+        fontWeight: FontWeight.w400,
+        color: CustomColors.textSecondary,
+      ),
+      activeIndicatorBorder: const BorderSide(color: CustomColors.primary),
+      suffixIconColor: CustomColors.textSecondary,
+      prefixIconColor: CustomColors.textSecondary,
+    ),
+    buttonTheme: const ButtonThemeData(buttonColor: CustomColors.primary),
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: CustomColors.primary,
+        foregroundColor: CustomColors.background,
+        overlayColor: CustomColors.accent,
+      ),
+    ),
+    outlinedButtonTheme: OutlinedButtonThemeData(
+      style: OutlinedButton.styleFrom(
+        backgroundColor: CustomColors.background,
+        foregroundColor: CustomColors.primary,
+        overlayColor: CustomColors.accent,
+      ),
+    ),
+  );
+
+  static OutlineInputBorder outlineInputBorder(final Color borderColor) {
+    return OutlineInputBorder(
+      borderRadius: BorderRadius.circular(8.0),
+      borderSide: BorderSide(color: borderColor),
+    );
+  }
+}
